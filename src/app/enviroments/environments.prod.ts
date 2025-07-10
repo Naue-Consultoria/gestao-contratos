79d85811e39fcadd058feed42d29eb035acccecd
Gabriel Damasceno <dmsc.prog@gmail.com>
2025-07-10 12:04:20 -0300

    feat: production url

--- src/app/enviroments/environments.prod.ts
+++ src/app/enviroments/environments.prod.ts
@@ -1,5 +1,6 @@
 export const environmentProd = {
   production: true,
-  apiUrl: 'https://your-production-api.com/api',
-  authUrl: 'https://your-production-api.com/api/auth'
+  apiUrl: 'https://gestao-contratos-backend-46ru.onrender.com',
+  authUrl: 'https://gestao-contratos-backend-46ru.onrender.com/auth',
 };
+// TODO: Replace with your production API URL and authentication URL.
